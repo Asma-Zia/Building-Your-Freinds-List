85623d02bd6f645bb690a401698e39cd5a9f5357
Asma zia <167691762+Asma-Zia@users.noreply.github.com>
2024-05-28 14:51:56 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+//------------------------------ ASSIGNMENT 1----------------------------------
+//==============================Buliding your friend list========================
+// TASK : Create a program that manages a simple friend list.
+var friend1 = {
+    firstName: "Vaniza",
+    lastName: "Choudry",
+    id: 408,
+};
+var friend2 = {
+    firstName: "Zunaira",
+    lastName: "Khan",
+    id: 566,
+};
+var friend3 = {
+    firstName: "Anas",
+    lastName: "Kaleem",
+    id: 352,
+};
+var people = {
+    friends: [friend1, friend2, friend3]
+};
+console.log(people);
---
+++ main.ts
@@ -0,0 +1,27 @@
+//------------------------------ ASSIGNMENT 1----------------------------------
+//==============================Buliding your friend list========================
+// TASK : Create a program that manages a simple friend list.
+
+let friend1={
+    firstName: "Vaniza",
+    lastName: "Choudry",
+    id: 408,
+}
+
+let friend2= {
+    firstName: "Zunaira",
+    lastName: "Khan",
+    id: 566,
+}
+
+let friend3= {
+    firstName: "Anas",
+    lastName: "Kaleem",
+    id: 352,
+}
+ let people={
+    friends: [friend1, friend2, friend3]
+ }
+
+ console.log(people);
+
